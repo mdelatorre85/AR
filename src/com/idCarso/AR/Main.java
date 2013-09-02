f011cdaea2932864214126189badb2a5080d4f0f
Miguel Angel De la Torre <miguel@MacBook-Pro-de-Miguel.local>
2013-09-02 11:32:19 -0500

    Se agrega la librería y el código de ejemplo

--- src/com/idCarso/AR/Main.java
+++ src/com/idCarso/AR/Main.java
@@ -1,10 +1,28 @@
 package com.idCarso.AR;
 
+import weka.associations.Apriori;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+/**
+ * 
+ * @author miguel
+ * 
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		// load data
+		Instances data = DataSource.read(args[0]);
+		data.setClassIndex(data.numAttributes() - 1);
 
+		// build associator
+		Apriori apriori = new Apriori();
+		apriori.setClassIndex(data.classIndex());
+		apriori.buildAssociations(data);
+
+		// output associator
+		System.out.println(apriori);
 	}
 
 }
