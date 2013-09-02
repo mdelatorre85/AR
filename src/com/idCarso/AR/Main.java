adf095fe5b00338ce76b11197be5d98a50ec1772
Miguel Angel De la Torre <miguel@MacBook-Pro-de-Miguel.local>
2013-09-02 16:30:49 -0500

    Funciona y muestra reglas de asociación apriori

--- src/com/idCarso/AR/Main.java
+++ src/com/idCarso/AR/Main.java
@@ -13,16 +13,22 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// load data
-		Instances data = DataSource.read(args[0]);
+//		Instances data = DataSource.read(args[0]);
+		Instances data = DataSource.read("/DATA.CSV");
 		data.setClassIndex(data.numAttributes() - 1);
 
 		// build associator
 		Apriori apriori = new Apriori();
 		apriori.setClassIndex(data.classIndex());
+		
+		apriori.setNumRules(30);
+		
 		apriori.buildAssociations(data);
+	
 
 		// output associator
 		System.out.println(apriori);
 	}
 
 }
+ 
